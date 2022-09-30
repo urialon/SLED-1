816065e6c1e320f97f4560e769906f2289583081
Uri Alon <urialon1@gmail.com>
2022-09-30 09:01:16 -0400

    Update hf_hub_download usage to newer versions
    In newer versions of the `huggingface_hub` library, the function `hf_hub_download` requires specifying:
    `repo_id="tau/scrolls", repo_type='dataset'`
    instead of `repo_id="datasets/tau/scrolls"`. 
    
    Otherwise, it will raise an error: `huggingface_hub.utils._validators.HFValidationError: Repo id must be in the form 'repo_name' or 'namespace/repo_name': 'datasets/tau/scrolls'. Use 'repo_type' argument if needed.`
    
    Tested on the newest version `0.10.0` of `huggingface_hub`. The new change also works on an older `0.8.1` version that accepts the old usage as well.

--- examples/seq2seq/metrics/metrics.py
+++ examples/seq2seq/metrics/metrics.py
@@ -148,7 +148,7 @@ class ScrollsWrapper(Metric):
 
 def download_metric():
     # here we load the custom metrics
-    scrolls_metric_path = hf_hub_download(repo_id="datasets/tau/scrolls", filename="metrics/scrolls.py")
+    scrolls_metric_path = hf_hub_download(repo_id="tau/scrolls", filename="metrics/scrolls.py", repo_type='dataset')
     updated_scrolls_metric_path = (
         os.path.dirname(scrolls_metric_path) + os.path.basename(scrolls_metric_path).replace(".", "_") + ".py"
     )
